92900080d3b518a75d082eb9c577267b2ad0a883
thanhnhnta <88866708+thanhnhnta@users.noreply.github.com>
2022-04-06 16:42:25 +0700

    test

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -46,8 +46,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            $user = $request->user();
-            return Limit::perMinute(60)->by(($user ?? $user->id) ?: $request->ip());
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
     }
 }
